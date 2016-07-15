4f13601e43ed92fd8fbc2136cafec5701beca5b9
mmann1123 <mmann1123@node167.cm.cluster>
2016-07-15 14:03:03 -0400

    updated cloud masking

--- DownloadMODISFTP_Rcurl.R
+++ DownloadMODISFTP_Rcurl.R
@@ -53,12 +53,12 @@ lapply(1:length(functions_in), function(x){cmpfun(get(functions_in[[x]]))})  # b
   # Product Filters 
   products =  c('MYD13Q1','MOD13Q1')  #EVI c('MYD13Q1','MOD13Q1')  , land cover = 'MCD12Q1' for 250m and landcover ='MCD12Q2'
   location = c(30.259,75.644)  # Lat Lon of a location of interest within your tiles listed above #India c(-31.467934,-57.101319)  #
-  tiles =   c('h13v12')  # uruguay c('h13v12')   # India example c('h24v05','h24v06'), Uruguay h13v12  
+  tiles =   c('h24v05','h24v06')  # uruguay c('h13v12')   # India example c('h24v05','h24v06'), Uruguay h13v12  
   dates = c('2002-01-01','2016-02-02') # example c('year-month-day',year-month-day') c('2002-07-04','2016-02-02') 
   ftp = 'ftp://ladsweb.nascom.nasa.gov/allData/6/'    # allData/6/ for evi, 
   # allData/51/ for landcover DOESn't WORK jUST PULL FROM FTP
   #strptime(gsub("^.*A([0-9]+).*$", "\\1",GetDates(location[1], location[2],products[1])),'%Y%j') # get list of all available dates for products[1]
-  out_dir = '/groups/manngroup/India_Index/Data/Uruguay/'
+  out_dir = '/groups/manngroup/India_Index/Data/India/' #/groups/manngroup/India_Index/Data/Uruguay/
   setwd(out_dir)
   
  
@@ -351,13 +351,15 @@ lapply(1:length(functions_in), function(x){cmpfun(get(functions_in[[x]]))})  # b
 
   # Loop through valid ranges  
   products2clean = unique(valid$stack)
+
   for(product in products2clean){
   for( tile in tiles){
         print(paste('Working on',product,tile))	
         # load product data
         lapply(dir1[grep(product,dir1)],load,.GlobalEnv) 
         data_stackvalues = get(paste(product,'_stack_',tile,sep=''))
-        valid_values = valid[grep(product,valid$stack),]
+        data_stackvalues_names = names(data_stackvalues)
+	valid_values = valid[grep(product,valid$stack),]
 
 	ScaleClean = function(x){
         	x[x==as.numeric(valid_values$fill)]=NA
@@ -365,19 +367,26 @@ lapply(1:length(functions_in), function(x){cmpfun(get(functions_in[[x]]))})  # b
         	x[x > as.numeric(valid_values$validU)]=NA
         	# DONT SCALE THIS INCREASES DATA SIZE BY HUGE AMOUNT x = x * as.numeric(valid_values$scale)
         	x}
-	
+
+ 	#  (can't do replacement inside of dopar loop)
+	dir.create(file.path('../Data Stacks/WO Clouds Crops Clean/Rasters/'), showWarnings=F,recursive=T)
         junk = foreach(i=1:dim(data_stackvalues)[3]) %dopar% {
-                data_stackvalues[[i]]=ScaleClean(data_stackvalues[[i]])
-                return(i) # store some junk
-		}
+		 print(paste(product,tile,i))
+		 writeRaster(ScaleClean(data_stackvalues[[i]]),
+			paste('WO Clouds Crops Clean/Rasters/','',sprintf("%04d", i),'_',product,'_',tile,
+				'_',names(data_stackvalues[[i]]),'.tif',sep=''),overwrite=T )
+                 return(i) # return
+                }
+	data_stackvalues = stack(list.files('../Data Stacks/WO Clouds Crops Clean/Rasters/',
+		pattern=paste('_',product,'_',tile,sep=''),full.names=T))
+	names(data_stackvalues) = data_stackvalues_names
+       # dont delete folnder unlink("../Data Stacks/WO Clouds Crops Clean/Rasters/", recursive = TRUE) 
 
         assign(paste(product,'_stack_',tile,sep=''),data_stackvalues)
         dir.create(file.path('../Data Stacks/WO Clouds Crops Clean'), showWarnings=F,recursive=T)
         save(list=paste(product,'_stack_',tile,sep=''),
                 file = paste('WO Clouds Crops Clean/',product,'_stack_',tile,'_wo_clouds_crops_clean.RData',sep=''))
-
         rm(list=ls()[grep(product,ls())])
-
   }}
 
 
@@ -493,4 +502,30 @@ lapply(1:length(functions_in), function(x){cmpfun(get(functions_in[[x]]))})  # b
   
   
 
+
+
+#       chunk_size = round(dim(data_stackvalues)[3]/4)
+#        junk = foreach(i=1:chunk_size) %dopar% {
+#                 return(ScaleClean(data_stackvalues[[i]]) ) # return
+#                }
+#        for(i in 1:chunk_size){data_stackvalues[[i]] = junk[[i]]}
+#       rm(junk)
+#        gc()
+#        junk = foreach(i=(chunk_size+1):(chunk_size*2)) %dopar% {
+#                 return(ScaleClean(data_stackvalues[[i]]) ) # return
+#                }
+#        for(i in (chunk_size+1):(chunk_size*2) ){data_stackvalues[[i]] = junk[[i]]}
+#        rm(junk)
+#
+#        junk = foreach(i=(chunk_size*2+1):(chunk_size*3)) %dopar% {
+#                 return(ScaleClean(data_stackvalues[[i]]) ) # return
+#                }
+#        for(i in (chunk_size*2+1):(chunk_size*3) ){data_stackvalues[[i]] = junk[[i]]}
+#        rm(junk)
+#
+#        junk = foreach(i=(chunk_size*3+1):dim(data_stackvalues)[3]) %dopar% {
+#                 return(ScaleClean(data_stackvalues[[i]]) ) # return
+#                }
+#        for(i in (chunk_size*3+1):dim(data_stackvalues)[3]) ){data_stackvalues[[i]] = junk[[i]]}
+#        rm(junk)
 
